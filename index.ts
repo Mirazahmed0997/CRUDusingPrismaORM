783ae4f2357a6c259b2c4a1cd4e4cfca66bc1c57
Miraz Ahmed <ahmedmiraz87@gmail.com>
2026-02-01 14:53:31 +0600

    CRUDWithPrisma

--- index.ts
+++ index.ts
@@ -1,18 +1,83 @@
+// npm install prisma@5 @prisma/client@5
+
+
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const user = await prisma.user.create({
-    data: {
-      name: "ISty",
-      email: "isty1@example.com",
-      profilePhoto: "https://images.unsplash.com/photo-1575936123452-b67c3203c357?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
+
+// creater data
+
+
+
+//   const user = await prisma.user.create({
+//     data: {
+//       name: "ISty",
+//       email: "isty1@example.com",
+//       profilePhoto: "https://images.unsplash.com/photo-1575936123452-b67c3203c357?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
+//     }
+//   });
+
+
+
+// find unique data by primary key
+
+// const user = await prisma.user.findUnique({
+//     where: {
+//         id: 1
+//     }
+// });
+
+
+// find all data
+
+// const user = await prisma.user.findMany({
+    
+// });
+
+
+
+// update data
+
+// const user = await prisma.user.update({
+//     where: {
+//         id: 1
+//     },
+//     data: {
+//         name: "Miraz Ahmed",
+//         email: "miraz.ahmed.updated@example.com"
+//     }
+// });
+
+
+// update data by non-primary field
+// const user = await prisma.user.update({
+//     where: {
+//         profilePhoto: null
+//     },
+//     data: {
+//        profilePhoto: "https://images.unsplash.com/photo-1575936123452-b67c3203c357?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D"
+//     }
+// });
+
+// delete data
+
+
+const user = await prisma.user.delete({
+    where: {
+        id: 1
     }
-  });
+});
 
   console.log(user);
 }
+
+
+
+
+
+
 
 main()
   .catch(console.error)
